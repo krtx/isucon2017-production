2086fab321ab3aeaf2d06799f185e1884fd9b8d1
ichyo <ichyo00@gmail.com>
2017-10-22 17:20:37 +0900

    Add cache

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -43,6 +43,38 @@ func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return r.templates.ExecuteTemplate(w, name, data)
 }
 
+var (
+	havereadC = newHaveReadCache()
+)
+
+type HaveReadCacheKey struct {
+	userId int64
+	chanId int64
+}
+
+type HaveReadCache struct {
+	data map[HaveReadCacheKey]int64
+}
+
+func newHaveReadCache() *HaveReadCache {
+	return &HaveReadCache{
+		make(map[HaveReadCacheKey]int64),
+	}
+}
+func (c *HaveReadCache) Clear() {
+	c.data = make(map[HaveReadCacheKey]int64)
+}
+func (c *HaveReadCache) Set(userId int64, chanId int64, lastId int64) {
+	c.data[HaveReadCacheKey{userId, chanId}] = lastId
+}
+func (c *HaveReadCache) Get(userId int64, chanId int64) int64 {
+	if val, ok := c.data[HaveReadCacheKey{userId, chanId}]; ok {
+		return val
+	} else {
+		return 0
+	}
+}
+
 func init() {
 	seedBuf := make([]byte, 8)
 	crand.Read(seedBuf)
@@ -208,7 +240,7 @@ func getInitialize(c echo.Context) error {
 	db.MustExec("DELETE FROM image WHERE id > 1001")
 	db.MustExec("DELETE FROM channel WHERE id > 10")
 	db.MustExec("DELETE FROM message WHERE id > 10000")
-	db.MustExec("DELETE FROM haveread")
+	havereadC.Clear()
 	return c.String(204, "")
 }
 
@@ -398,10 +430,7 @@ func getMessage(c echo.Context) error {
 	}
 
 	if len(messages) > 0 {
-		_, err := db.Exec("INSERT INTO haveread (user_id, channel_id, message_id, updated_at, created_at)"+
-			" VALUES (?, ?, ?, NOW(), NOW())"+
-			" ON DUPLICATE KEY UPDATE message_id = ?, updated_at = NOW()",
-			userID, chanID, messages[0].ID, messages[0].ID)
+		havereadC.Set(userID, chanID, messages[0].ID)
 		if err != nil {
 			return err
 		}
@@ -417,24 +446,7 @@ func queryChannels() ([]int64, error) {
 }
 
 func queryHaveRead(userID, chID int64) (int64, error) {
-	type HaveRead struct {
-		UserID    int64     `db:"user_id"`
-		ChannelID int64     `db:"channel_id"`
-		MessageID int64     `db:"message_id"`
-		UpdatedAt time.Time `db:"updated_at"`
-		CreatedAt time.Time `db:"created_at"`
-	}
-	h := HaveRead{}
-
-	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
-		userID, chID)
-
-	if err == sql.ErrNoRows {
-		return 0, nil
-	} else if err != nil {
-		return 0, err
-	}
-	return h.MessageID, nil
+	return havereadC.Get(userID, chID), nil
 }
 
 func fetchUnread(c echo.Context) error {
